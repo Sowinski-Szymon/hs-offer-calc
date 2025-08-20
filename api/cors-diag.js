cc709359e58f9ac43635c3d357240c816165233a
Sowinski-Szymon <sz.sowinski@publink.com>
2025-08-20 10:43:24 +0200

    Update cors-diag.js

--- api/cors-diag.js
+++ api/cors-diag.js
@@ -1,8 +1,9 @@
-// CommonJS
-module.exports = (req, res) => {
+const { withCORS } = require('./_lib/cors');
+
+module.exports = withCORS((req, res) => {
   res.status(200).json({
     originHeader: req.headers.origin || null,
     allowedOriginsEnv: process.env.ALLOWED_ORIGINS || process.env.ALLOWED_ORIGIN || null,
     vercelEnv: process.env.VERCEL_ENV || null
   });
-};
+});
