77094f0e1ab60049ed7a59b501a0f894b8bc6576
Sowinski-Szymon <sz.sowinski@publink.com>
2025-08-26 12:08:13 +0200

    Delete api/cors-diag.js

--- api/cors-diag.js
+++
@@ -1,9 +0,0 @@
-const { withCORS } = require('./_lib/cors');
-
-module.exports = withCORS((req, res) => {
-  res.status(200).json({
-    originHeader: req.headers.origin || null,
-    allowedOriginsEnv: process.env.ALLOWED_ORIGINS || process.env.ALLOWED_ORIGIN || null,
-    vercelEnv: process.env.VERCEL_ENV || null
-  });
-});
